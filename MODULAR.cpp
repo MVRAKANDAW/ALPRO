2340c8c414cb11b70310c860fc98407f9f429692
MVRAKANDAW <mochammadvianto@gmail.com>
2017-03-26 12:09:10 +0700

    E-Learning WEEK 26 Maret 2017

---
+++ MODULAR.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+// {i.s} Inisialisasi Fungsi
+// {f.s} Pemanggilan Fungsi
+using namespace std;
+
+// Inisialisasi Fungsi
+ void contoh() {
+  cout << "Ini Hanya Contoh " << endl;
+
+}
+
+//Inisialisai Fungsi
+ void Nama(string nama) {
+  cout << "Haiii Lur" << nama << endl;
+}
+
+int main(){
+
+  contoh(); // pemanggilan fungsi
+
+
+  Nama("Moch Vianto Rakanda"); // pemanggilan fungsi
+
+  cout << "Selamat Datang" << endl;
+
+  return 0;
+}
