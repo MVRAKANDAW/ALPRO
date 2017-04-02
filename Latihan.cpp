6ea3eaa9666dbfcbcff87f141b78931895080c38
MVRAKANDAW <mochammadvianto@gmail.com>
2017-04-02 12:36:30 +0700

    Latihan Soal 2 April 2017

---
+++ Latihan.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//void pilihanA() {
+
+
+
+//}
+
+//void pilihanB() {
+
+
+
+
+//}
+
+int main () {
+
+int angka = 0 , max, min, jumlah = 0 ,  pil;
+char A, B;
+
+A = pil;
+B = pil;
+
+cout << "Masukan Jumlah : ";
+cin >> jumlah ;
+
+//cout << "Masukan pilihan : ";
+//cin >> pil ;
+
+while (angka >= 0 ) {
+
+if ((pil >= B)) {B=pil;}
+
+else if((pil <= A)) {A=pil;} angka++;
+
+cout << "Masukan angka : ";
+cin >> angka ;
+
+}
+
+
+  cout << "Nilai Minimum          : " << pil << endl;
+  cout << "Nilai Maximum           : " << pil << endl;
+
+
+
+return 0;
+}
